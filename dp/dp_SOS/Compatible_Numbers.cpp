8f378c7c524528fda34a13c75da6f49687de2cd0
Raghad Islam <raghad.noser.2003@gmail.com>
2025-08-01 01:12:32 +0300

    solve Problems using Sum of Subsets DP

---
+++ dp/dp_SOS/Compatible_Numbers.cpp
@@ -0,0 +1,67 @@
+/* problem link: https://codeforces.com/contest/165/problem/E
+ * solution by: Raghad Islam
+ * date: 27-7-2025 
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+void fileIO(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void fastIO(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+typedef long long ll;
+#define int ll
+
+// B -> number of bits
+const int B = 22, M = 1 << B;
+
+// subset contribute to its superset
+void solveSub(vector<int>& dp) {
+    for (int i = 0; i < B; ++i)
+        for (int m = 0; m < M; ++m)
+            if ( (m & (1 << i)) && (dp[m ^ (1 << i)] != -1) )
+                dp[m] = dp[m ^ (1 << i)];
+}
+
+// superset contribute to its subset
+void solveSup(vector<int>& dp) {
+    for (int i = 0; i < B; ++i)
+        for (int m = M - 1; ~m; --m)
+            if ( m & (1 << i) )
+                dp[m ^ (1 << i)] += dp[m];
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<int> dp1(M, -1);
+    vector<int> x(n);
+    for (int i = 0; i < n; ++i) {
+         cin >> x[i];
+        dp1[x[i]] = x[i];
+    }
+    solveSub(dp1);
+    for (int i = 0; i < n; ++i) {
+        cout << dp1[x[i] ^ (M-1)] << ' ';
+    }
+}
+
+signed main(){
+    fileIO();
+    fastIO();
+    int t = 1;// cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
